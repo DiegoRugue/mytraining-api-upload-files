c18c02af8b221d922a7736148572d2e25c0d9ce3
diegorugue <diego.rugue@smn.com.br>
2019-11-04 16:48:54 -0300

    upload images

---
+++ src/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const fs = require('fs');
+const { join } = require('path');
+const uuid = require('uuid/v4');
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/file', async (req, res) => {
+    let file = req.body.file;
+
+    if (file.includes(';base64,')) {
+        file = file.split(';base64,').pop();
+    }
+
+    const convertedFile = Buffer.from(file, 'base64');
+
+    const path = join(process.cwd(), 'uploads');
+
+    const filename = `${uuid()}-${Date.now()}.png`;
+
+    fs.writeFile(join(path, filename), convertedFile, err => {
+        if (err) res.json({ error: err });
+
+        res.json({ path: filename });
+    });
+});
+
+app.use(express.static(join(process.cwd(), 'uploads')));
+
+module.exports = app;
---
+++ src/server.js
@@ -0,0 +1,5 @@
+const app = require('./app');
+
+app.listen(3340, () => {
+    console.log('API is running on 3340');
+});
